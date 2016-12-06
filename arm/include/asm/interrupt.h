0703ab42f9095491ca25ae742e904cc99a03a7fd
Charleye <wangkart@aliyun.com>
2016-12-06 17:39:50 +0800

    Add new file

---
+++ arm/include/asm/interrupt.h
@@ -0,0 +1,51 @@
+#ifndef __INTERRUPT_H__
+#define __INTERRUPT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <linux/types.h>
+#include <linux/stddef.h>
+#include <linux/malloc.h>
+#include <linux/stdlib.h>
+#include <linux/string.h>
+#include <linux/list.h>
+#include <io.h>
+
+enum irq_type_t {
+	IRQ_TYPE_NONE			= 0,
+	IRQ_TYPE_LEVEL_LOW		= 1,
+	IRQ_TYPE_LEVEL_HIGH		= 2,
+	IRQ_TYPE_EDGE_FALLING	= 3,
+	IRQ_TYPE_EDGE_RISING	= 4,
+	IRQ_TYPE_EDGE_BOTH		= 5,
+};
+
+struct irq_handler_t {
+	void (*func)(void * data);
+	void * data;
+};
+
+struct irq_t {
+	const char * name;
+	const int no;
+	struct irq_handler_t * handler;
+
+	void (*enable)(struct irq_t * irq);
+	void (*disable)(struct irq_t * irq);
+	void (*set_type)(struct irq_t * irq, enum irq_type_t type);
+};
+
+bool_t irq_register(struct irq_t * irq);
+bool_t irq_unregister(struct irq_t * irq);
+bool_t request_irq(const char * name, void (*func)(void *), enum irq_type_t type, void * data);
+bool_t free_irq(const char * name);
+void enable_irq(const char * name);
+void disable_irq(const char * name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __INTERRUPT_H__ */
