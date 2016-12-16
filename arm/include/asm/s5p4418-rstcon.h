043945b71e3294748d43996b6a216a6842ee0b13
Charleye <wangkart@aliyun.com>
2016-12-16 14:11:43 +0800

    Modify error for the file

--- arm/include/asm/s5p4418-rstcon.h
+++ arm/include/asm/s5p4418-rstcon.h
@@ -13,77 +13,80 @@ extern "C" {
 #include <io.h>
 
 enum {
+    /* IP RESET REG0 */
 	RESET_ID_AC97			= 0,
 	RESET_ID_CPU1			= 1,
 	RESET_ID_CPU2			= 2,
 	RESET_ID_CPU3			= 3,
-	RESET_ID_WD1 			= 4,
+	RESET_ID_WD1 			= 4,    /* Watch Dog Timer1 Reset */
 	RESET_ID_WD2 			= 5,
 	RESET_ID_WD3 			= 6,
-	RESET_ID_CRYPTO 		= 7,
-	RESET_ID_DEINTERLACE 	= 8,
-	RESET_ID_DISP_TOP		= 9,
-	RESET_ID_DISPLAY		= 10,
-	RESET_ID_RESCONV		= 11,
-	RESET_ID_LCDIF			= 12,
-	RESET_ID_HDMI			= 13,
-	RESET_ID_HDMI_VIDEO		= 14,
-	RESET_ID_HDMI_SPDIF		= 15,
-	RESET_ID_HDMI_TMDS		= 16,
-	RESET_ID_HDMI_PHY		= 17,
-	RESET_ID_LVDS			= 18,
-	RESET_ID_ECID			= 19,
-	RESET_ID_I2C0			= 20,
-	RESET_ID_I2C1			= 21,
-	RESET_ID_I2C2			= 22,
-	RESET_ID_I2S0			= 23,
-	RESET_ID_I2S1			= 24,
-	RESET_ID_I2S2			= 25,
-	RESET_ID_DREX_C			= 26,
-	RESET_ID_DREX_A			= 27,
-	RESET_ID_DREX			= 28,
-	RESET_ID_MIPI			= 29,
-	RESET_ID_MIPI_DSI		= 30,
-	RESET_ID_MIPI_CSI		= 31,
+	RESET_ID_CRYPTO 		= 7,    /* Crypto Engine Reset */
+	RESET_ID_DEINTERLACE 	= 8,    /* Deinterlace Reset */
+	RESET_ID_DISP_TOP		= 9,    /* Display Block Total */
+	RESET_ID_DISPLAY		= 10,   /* Dual Display (MLC & DPC) */
 
-	RESET_ID_MIPI_PHY_S		= 32,
-	RESET_ID_MIPI_PHY_M		= 33,
-	RESET_ID_MPEGTSI		= 34,
-	RESET_ID_PDM			= 35,
-	RESET_ID_TIMER			= 36,
-	RESET_ID_PWM			= 37,
-	RESET_ID_SCALER			= 38,
-	RESET_ID_SDMMC0			= 39,
-	RESET_ID_SDMMC1			= 40,
-	RESET_ID_SDMMC2			= 41,
-	RESET_ID_SPDIFRX		= 42,
-	RESET_ID_SPDIFTX		= 43,
-	RESET_ID_SSP0_P			= 44,
-	RESET_ID_SSP0			= 45,
-	RESET_ID_SSP1_P			= 46,
-	RESET_ID_SSP1			= 47,
-	RESET_ID_SSP2_P			= 48,
-	RESET_ID_SSP2			= 49,
-	RESET_ID_UART0			= 50,
-	RESET_ID_UART1			= 51,
-	RESET_ID_UART2			= 52,
-	RESET_ID_UART3			= 53,
-	RESET_ID_UART4			= 54,
-	RESET_ID_UART5			= 55,
-	RESET_ID_USB20HOST		= 56,
-	RESET_ID_USB20OTG		= 57,
-	RESET_ID_WDT			= 58,
-	RESET_ID_WDT_POR		= 59,
-	RESET_ID_ADC			= 60,
-	RESET_ID_CODA_A			= 61,
-	RESET_ID_CODA_P			= 62,
-	RESET_ID_CODA_C			= 63,
+//	RESET_ID_RESCONV		= 11,
+//	RESET_ID_LCDIF			= 12,
+	RESET_ID_HDMI			= 13,   /* HDMI LINK & CEC */
+	RESET_ID_HDMI_VIDEO		= 14,   /* HDMI LINK VIDEO Block */
+	RESET_ID_HDMI_SPDIF		= 15,   /* HDMI LINK SPDIF Block */
+	RESET_ID_HDMI_TMDS		= 16,   /* HDMI LINK TMDS Block */
+	RESET_ID_HDMI_PHY		= 17,   /* HDMI PHY */
+	RESET_ID_LVDS			= 18,   /* LVDS PHY */
+	RESET_ID_I2C0			= 19,
+	RESET_ID_I2C1			= 20,
+	RESET_ID_I2C2			= 21,
+	RESET_ID_I2S0			= 22,
+	RESET_ID_I2S1			= 23,
+	RESET_ID_I2S2			= 24,
+	RESET_ID_MCUYZTOP_C		= 25,   /* Memory Controller APB Interface */
+	RESET_ID_MCUYZTOP_A		= 26,   /* Memory Controller AXI Interface */
+	RESET_ID_MCUYZTOP   	= 27,   /* Memory Controller Core */
+	RESET_ID_MIPI			= 28,   /* MIPI Register Interface */
+	RESET_ID_MIPI_DSI		= 29,   /* MIPI DSI Master */
+	RESET_ID_MIPI_CSI		= 30,   /* MIPI CSI Slave */
+    RESET_ID_MIPI_PHY_S     = 31,   /* MIPI D-PHY Slave Channel */
 
-	RESET_ID_DWC_GMAC		= 64,
-	RESET_ID_VR				= 65,
-	RESET_ID_PPM			= 66,
-	RESET_ID_VIP0			= 67,
-	RESET_ID_VIP1			= 68,
+    /* IP RESET REG1 */
+	RESET_ID_MIPI_PHY_M		= 32,   /* MIPI D-PHY Master Channel */
+	RESET_ID_MPEGTSI		= 33,   /* MPEG-TSI */
+	RESET_ID_PDM			= 34,
+	RESET_ID_PWMTIMER0		= 35,   /* PWMTIMER0 */
+	RESET_ID_PWMTIMER1		= 36,
+	RESET_ID_SCALER			= 37,
+	RESET_ID_SDMMC0			= 38,   /* SDMMC Controller 0 */
+	RESET_ID_SDMMC1			= 39,
+	RESET_ID_SDMMC2			= 40,
+	RESET_ID_SPDIFRX		= 41,
+	RESET_ID_SPDIFTX		= 42,
+	RESET_ID_SSP0_P			= 43,   /* SSP0 APB Interface */
+	RESET_ID_SSP0			= 44,   /* SSP0 Core */
+	RESET_ID_SSP1_P			= 45,
+	RESET_ID_SSP1			= 46,
+	RESET_ID_SSP2_P			= 47,
+	RESET_ID_SSP2			= 48,
+	RESET_ID_UART0			= 49,
+	RESET_ID_UART1			= 50,
+	RESET_ID_UART2			= 51,
+	RESET_ID_UART3			= 52,
+	RESET_ID_UART4			= 53,
+
+	RESET_ID_USB20HOST		= 55,   /* USB 2.0 Host */
+	RESET_ID_USB20OTG		= 56,   /* USB 2.0 OTG */
+	RESET_ID_WDT			= 57,   /* WDT APB */
+	RESET_ID_WDT_POR		= 58,
+	RESET_ID_ADC			= 59,
+	RESET_ID_CODA_A			= 60,   /* Multi-Format Video Codec AXI Interface */
+	RESET_ID_CODA_P			= 61,   /* Multi-Format Video Codec APB Interface */
+	RESET_ID_CODA_C			= 62,   /* Multi-Format Video Codec Core */
+    RESET_ID_DWC_GMAC		= 63,
+
+    /* IP RESET REG2 */
+    RESET_3DGPU             = 64,   /* 3D GPU */
+	RESET_ID_PPM			= 65,
+	RESET_ID_VIP0			= 66,   /* VIP Controller 0 */
+	RESET_ID_VIP1			= 67,
 };
 
 void s5p4418_ip_reset(int id, int force);
